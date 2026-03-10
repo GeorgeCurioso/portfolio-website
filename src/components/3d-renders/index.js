ba89dfb38b8f09f549fe94a3f321fd6c40a82f88
GeorgeCurioso <275589762+GeorgeCurioso@users.noreply.github.com>
2026-03-10 00:28:10 -0600

    3D Model Chosen

--- src/components/3d-renders/index.js
+++ src/components/3d-renders/index.js
@@ -2,10 +2,7 @@
 // 3D RENDERS COMPONENTS
 // ===============================
 
+export { default as SketchfabModel } from "./Rhetorician";
 export { default as TrooperCanvas } from "./Trooper";
 export { default as BallCanvas } from "./Ball";
 export { default as StarsCanvas } from "./Stars";
-
-export { default as SketchfabModel } from "./Rhetorician";
-//export { default as SketchfabModel } from "./R2d2";
-//export { default as SketchfabModel } from "./Pacman";
